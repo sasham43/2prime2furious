3a2674b2de2a10324ddfaf11cd48c7217f943b0b
sasham43 <sashasemail@gmail.com>
2016-03-30 08:36:46 -0500

    Add ability to create new buildings and retain their collision-ness

--- app.js
+++ app.js
@@ -9,51 +9,6 @@ $(function(){
 
 //  $(".box:first").add(".box:last").toggleClass("invisible");
 
-
-  // basket
-  var bX = $(".basket").css("top");
-  var bY = $(".basket").css("left");
-
-  // box-follow
-  var boxSelected = '';
-  var scored = false;
-  $(".box").one('click', function(event){
-    //$(event.target).toggleClass("absolute-positioned"); // switch positioning to absolute so the left and top properties affect it
-    //$(event.target).text(event.target.id);
-    boxSelected = "#" + event.target.id;
-    // console.log(boxSelected);
-  });
-
-  $(".box").on('dblclick',function(){
-    boxSelected = '';
-    scored = false;
-  });
-
-  var points = 0;
-  $(document).on("mousemove",function(event){
-    var xPos = event.pageX;
-    var yPos = event.pageY;
-    $("#xPos").html(xPos);
-    $("#yPos").html(yPos);
-    $("#xBasket").html(bX);
-    $("#yBasket").html(bY);
-    // console.log('xPos',xPos);
-    // console.log('yPos',yPos);
-     $(boxSelected).css({"left":(event.pageX)-50, "top":(event.pageY)-50}); // makes it so the box follows the cursor.  The -50 is so that it's possible to click an element to return it to its original position
-
-     // scorekeeping
-     if(!scored & boxSelected != ''){
-       if(xPos >= 240 && xPos <= 293){
-         if(yPos >= 539 && yPos <= 594){
-           scored = true;
-           points++;
-           $("#points").html(points);
-         }
-       }
-     }
-  });
-
-
 //////////////////////////////
 //      cars
 ///////////////////////////////
@@ -64,20 +19,33 @@ $(function(){
   var carClicked = '#car1';
   $(".cars").on('click',function(event){
     //console.log(event.target);
-    carClicked = '#' + event.target.id;
+    carClicked = '#' + event.target.id; // grab id and add # selector for jQuery
     //console.log(carClicked);
   });
 
-  // construct building objects
-  var buildingArray = [];
-  $(".building").each(function(index){
-    var tempX = parseInt($(this).css("left"));
-    var tempY = parseInt($(this).css("top"));
-    var tempWidth = parseInt($(this).css("width"));
-    var tempHeight = parseInt($(this).css("height"));
-    var tempBuilding = new Building(tempX, tempY, tempWidth, tempHeight);
+  // press 'n' to create new building
 
-    buildingArray.push(tempBuilding);
+  $(document).keydown(function(key){
+
+  });
+
+
+  // click to move buildings
+  var buildingSelected = '';
+  $(document).on('click',".building",function(event){
+    buildingSelected = '#' + event.target.id;
+    console.log(buildingSelected);
+  });
+
+  $(document).on('dblclick', ".building" ,function(){
+    buildingSelected = '';
+  })
+
+  $(document).on('mousemove',function(event){
+    var buildingX = event.pageX;
+    var buildingY = event.pageY;
+
+    $(buildingSelected).css({"left":(event.pageX)-50, "top":(event.pageY)-50});
   });
 
   // console.log("buildingArray",buildingArray);
@@ -93,34 +61,50 @@ $(function(){
       height: parseInt($(carClicked).css("height"))
     };
 
+    // construct building objects
+    var buildingArray = [];
+    $(".building").each(function(index){
+      var tempX = parseInt($(this).css("left"));
+      var tempY = parseInt($(this).css("top"));
+      var tempWidth = parseInt($(this).css("width"));
+      var tempHeight = parseInt($(this).css("height"));
+      var tempBuilding = new Building(tempX, tempY, tempWidth, tempHeight);
+
+      buildingArray.push(tempBuilding);
+    });
+
     var keyX = parseInt($(carClicked).css("left"));
     var keyY = parseInt($(carClicked).css("top"));
+
+    // pull out speed and bounceback variables
+    var speed = 15;
+    var bounceback = 10;
 
     if(!collisionDetected){
       switch(key.which){
         case 38: // up
-          keyY -= 15;
+          keyY -= speed;
           $(carClicked).addClass("car-up");
           $(carClicked).removeClass("car-right");
           $(carClicked).removeClass("car-left");
           $(carClicked).removeClass("car-down");
           break;
         case 40: // down
-          keyY += 15;
+          keyY += speed;
           $(carClicked).addClass("car-down");
           $(carClicked).removeClass("car-right");
           $(carClicked).removeClass("car-left");
           $(carClicked).removeClass("car-up");
           break;
         case 37: // left
-          keyX -= 15;
+          keyX -= speed;
           $(carClicked).addClass("car-left");
           $(carClicked).removeClass("car-down");
           $(carClicked).removeClass("car-right");
           $(carClicked).removeClass("car-up");
           break;
         case 39: // right
-          keyX += 15;
+          keyX += speed;
           $(carClicked).addClass("car-right");
           $(carClicked).removeClass("car-left");
           $(carClicked).removeClass("car-down");
@@ -130,16 +114,16 @@ $(function(){
     } else {
       switch(key.which){
         case 38:
-          keyY+=5;
+          keyY+=bounceback;
           break;
         case 40:
-          keyY-=5;
+          keyY-=bounceback;
           break;
         case 37:
-          keyX+=5;
+          keyX+=bounceback;
           break;
         case 39:
-          keyX-=5;
+          keyX-=bounceback;
           break;
       }
       keyX-=5;
@@ -150,33 +134,6 @@ $(function(){
     var keyXString = keyX + "px";
     var keyYString = keyY + "px";
     $(carClicked).css({"left": keyXString,"top":keyYString});
-
-    // collision
-    // var car = {
-    //   x: parseInt($(carClicked).css("left")),
-    //   y: parseInt($(carClicked).css("top")),
-    //   width: parseInt($(carClicked).css("width")),
-    //   height: parseInt($(carClicked).css("height"))
-    // };
-
-    // var building = {
-    //   x: parseInt($(".building").css("left")),
-    //   y: parseInt($(".building").css("top")),
-    //   width: parseInt($(".building").css("width")),
-    //   height: parseInt($(".building").css("height"))
-    // };
-
-    // console.log('car',car);
-    // console.log('building',building);
-
-    // if(
-    //   car.x < building.x + building.width &&
-    //   car.x + car.width > building.x &&
-    //   car.y < building.y + building.height &&
-    //   car.y + car.height > building.y
-    // ) {
-    //   collisionDetected = true;
-    // }
 
     for(var kt = 0; kt < buildingArray.length; kt++){
       // console.log('buildingArray[kt]',buildingArray[kt]);
@@ -194,6 +151,14 @@ $(function(){
       }
     }
 
+    // create new buildings
+    // console.log(key.which);
+    var numBuildings = buildingArray.length;
+    var newID = numBuildings+1;
+    if(key.which == 78){
+      $(".building-container").append("<div class=\"building\" id=\"building" + newID + "\"></div>");
+    }
+
     // debug console
     // $("#carX").html(car.x);
     // $("#carY").html(car.y);
@@ -206,20 +171,12 @@ $(function(){
     // $("#buildingHeight").html(building.height);
   });
 
-
+  // building constructor
   function Building(x, y, width, height){
     this.x = x;
     this.y = y;
     this.width = width;
     this.height = height;
-    // this.isCollided = function(){
-    //   var collided = car.x < this.x + this.width &&
-    //   car.x + car.width > this.x &&
-    //   car.y < this.y + this.height &&
-    //   car.y + car.height > this.y;
-    //
-    //   return collided;
-    // }
   }
 
 
